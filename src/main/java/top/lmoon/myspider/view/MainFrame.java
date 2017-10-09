dbf368ab10fee635a44ab4148aca5e34b4a49f01
LMoon <LMoon@DESKTOP-K5U9EVU>
2017-10-10 00:06:55 +0800

    123

--- src/main/java/top/lmoon/myspider/view/MainFrame.java
+++ src/main/java/top/lmoon/myspider/view/MainFrame.java
@@ -9,6 +9,8 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.net.URI;
 import java.util.List;
 
@@ -16,12 +18,12 @@ import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.ListSelectionModel;
-import javax.swing.WindowConstants;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
@@ -29,8 +31,10 @@ import javax.swing.table.DefaultTableModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import top.lmoon.myspider.apefile.FileReader;
 import top.lmoon.myspider.constant.SysConstants;
+import top.lmoon.myspider.dao.ApeInfoDAO;
+import top.lmoon.myspider.dao.ApeInfoDAOH2DBImpl;
+import top.lmoon.myspider.h2db.H2DBServer;
 import top.lmoon.myspider.vo.ApeInfoVO;
 
 /**
@@ -38,56 +42,69 @@ import top.lmoon.myspider.vo.ApeInfoVO;
  * @date 2017年9月30日
  * 
  */
-public class MainFrame extends JFrame{
+public class MainFrame extends JFrame {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -8826403470545143680L;
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(MainFrame.class);
-	
+
 	private JTextArea textArea;
-	
+
 	private JTable jTable;
-	
+
 	private DefaultTableModel tableModel;
 	
+	private static int pageNo = 1;
+
 	private Desktop desktop = Desktop.getDesktop();
-	
+
 	private static final int VO_COLUMN_INDEX = 4;
 	
-	private void drawFrame(){
-		MainFrame mFrame = new MainFrame();
+	private static final String[] COLUMN_NAME = { "序号", "歌手", "歌名", "大小", "" };
+	
+	private static final int PAGE_SIZE = 50;
+	
+	private static ApeInfoDAO dao = new ApeInfoDAOH2DBImpl();
+	
+	private static MainFrame instance = new MainFrame();
+	
+	private MainFrame(){
+		
+	}
+	
+	public static MainFrame getInstance(){
+		return instance;
+	}
+
+	private void drawFrame() {
+		MainFrame mFrame = getInstance();
 		mFrame.setTitle("My songs");
 		mFrame.setIconImage(new ImageIcon("./res/img/title.png").getImage());
 		mFrame.setSize(690, 600);
-		mFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);//界面关闭方式
-		mFrame.setLocationRelativeTo(null);//显示的界面居中
+		mFrame.addWindowListener(new CloseWindowListener());
+		;
+		// mFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);//界面关闭方式
+		mFrame.setLocationRelativeTo(null);// 显示的界面居中
 
 		JPanel contentPanel = new JPanel();
 		contentPanel.setLayout(new BorderLayout());
 		contentPanel.add(westPanel(), BorderLayout.CENTER);
 		contentPanel.add(eastPanel(), BorderLayout.EAST);
 		contentPanel.add(southPanel(), BorderLayout.SOUTH);
-		mFrame.setContentPane(contentPanel);		
+		mFrame.setContentPane(contentPanel);
 		mFrame.setVisible(true);
 	}
-	
-	private class createVOAction implements ActionListener {
 
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see
-		 * java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent
-		 * )
-		 */
+	private class downloadAction implements ActionListener {
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			try {
 				int row = jTable.getSelectedRow();
-				if(row==-1){
+				if (row == -1) {
 					textArea.setText("没有选中任何表!");
 					return;
 				}
@@ -96,62 +113,83 @@ public class MainFrame extends JFrame{
 			} catch (Exception e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
-				logger.error("",e1);
+				logger.error("", e1);
 			}
-//			try {
-//				// TODO Auto-generated method stub
-//				int[] selectedRows = jTable.getSelectedRows();
-//				if(selectedRows.length==0){
-//					textArea.append("没有选中任何表!\n");
-//					return;
-//				}
-//				List<String> list = new ArrayList<String>();
-//				for (int i : selectedRows) {
-//					list.add(tableModel.getValueAt(i, 1).toString());
-//				}
-//				System.out.println(list);
-//				Connection conn = ConnectionUtils
-//						.getConnection(getProperties());
-//				DbTableVOCodeGenerator codeGenerator = new DbTableVOCodeGenerator(
-//						conn);
-//				String[] a = new String[]{};
-//				BeanInfo[] beanInfos = codeGenerator.generate(
-//						"vo1", list.toArray(a));
-//				JavaFileUtils.createJavaFile(null, beanInfos);
-//				textArea.append("生成VO"+list+"完毕!\n");
-//			} catch (Exception e1) {
-//				// TODO Auto-generated catch block
-//				e1.printStackTrace();
-//			}
 
 		}
 
 	}
 	
-	private JPanel westPanel() {
-		JPanel westPanel = new JPanel();
-		westPanel.setLayout(new BorderLayout());
-		List<ApeInfoVO> readAllFiles = FileReader.readAllFiles();
-		Object[][] data = new Object[readAllFiles.size()][VO_COLUMN_INDEX+1];
-//		System.out.println(readAllFiles);
+	private class pageUpAction implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(--pageNo<1){
+				pageNo++;
+				JOptionPane.showMessageDialog(getInstance(), "上页没有数据了哦！", "提示",JOptionPane.INFORMATION_MESSAGE);
+				return;
+			}
+			Object[][] data = getData(pageNo);
+			if(data == null){
+				pageNo++;
+				JOptionPane.showMessageDialog(getInstance(), "上页没有数据了哦！", "提示",JOptionPane.INFORMATION_MESSAGE);
+				return;
+			}
+			tableModel.setDataVector(data, COLUMN_NAME);
+		}
+
+	}
+	
+	private class pageDownAction implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			Object[][] data = getData(++pageNo);
+			if(data == null){
+				pageNo--;
+				JOptionPane.showMessageDialog(getInstance(), "下页没有数据了哦！", "提示",JOptionPane.INFORMATION_MESSAGE);
+				return;
+			}
+			tableModel.setDataVector(data, COLUMN_NAME);
+		}
+
+	}
+	
+	private static Object[][] getData(int pageNo){
+		List<ApeInfoVO> readAllFiles = dao.select(pageNo, PAGE_SIZE, null, null);
+		if(readAllFiles==null||readAllFiles.isEmpty()){
+			return null;
+		}
+		Object[][] data = new Object[readAllFiles.size()][VO_COLUMN_INDEX + 1];
 		for (int i = 0; i < readAllFiles.size(); i++) {
 			ApeInfoVO vo = readAllFiles.get(i);
-			data[i][0] = i+1;
+			data[i][0] = i + 1;
 			data[i][1] = vo.getSinger();
 			data[i][2] = vo.getTitle();
 			data[i][3] = vo.getSize();
 			data[i][VO_COLUMN_INDEX] = vo;
 		}
-		tableModel = new DefaultTableModel(data, new String[] { "序号",
-		"歌手","歌名","大小","" });
-		jTable = new JTable(tableModel){
+		return data;
+	}
+
+	private JPanel westPanel() {
+		JPanel westPanel = new JPanel();
+		westPanel.setLayout(new BorderLayout());
+		// List<ApeInfoVO> readAllFiles = FileReader.readAllFiles();
+		Object[][] data = getData(1);
+		if(data == null){
+			JOptionPane.showMessageDialog(getInstance(), "没有数据哦！", "提示",JOptionPane.INFORMATION_MESSAGE);
+		}
+		tableModel = new DefaultTableModel(data,COLUMN_NAME );
+		jTable = new JTable(tableModel) {
 			private static final long serialVersionUID = 1L;
+
 			@Override
 			public boolean isCellEditable(int row, int column) {
 				// TODO Auto-generated method stub
 				return false;
 			}
-			
+
 		};
 		jTable.getColumnModel().getColumn(0).setMaxWidth(40);
 		jTable.getColumnModel().getColumn(1).setMaxWidth(150);
@@ -166,84 +204,101 @@ public class MainFrame extends JFrame{
 
 		return westPanel;
 	}
-	
-	private ListSelectionListener listSelectionListener = new ListSelectionListener(){
+
+	private ListSelectionListener listSelectionListener = new ListSelectionListener() {
 
 		@Override
 		public void valueChanged(ListSelectionEvent e) {
 			try {
-//				int row = e.getLastIndex();
+				// int row = e.getLastIndex();
 				int row = jTable.getSelectedRow();
-				if(row==-1){
+				if (row == -1) {
 					textArea.setText("没有选中任何表!");
 					return;
 				}
-				//.getValueAt(row, 3)
+				// .getValueAt(row, 3)
 				ApeInfoVO vo = (ApeInfoVO) tableModel.getValueAt(row, VO_COLUMN_INDEX);
 				StringBuffer sb = new StringBuffer();
-				sb.append("歌手：").append(vo.getSinger()).append(SysConstants.COLUMN_SYMBOL).append(SysConstants.LINE_SYMBOL);
-				sb.append("歌曲：").append(vo.getTitle()).append(SysConstants.COLUMN_SYMBOL).append(SysConstants.LINE_SYMBOL);
-				sb.append("专辑：").append(vo.getAlbum()).append(SysConstants.COLUMN_SYMBOL).append(SysConstants.LINE_SYMBOL);
-				sb.append("大小：").append(vo.getSize()).append(SysConstants.COLUMN_SYMBOL).append(SysConstants.LINE_SYMBOL);
-				sb.append("语言：").append(vo.getLanguage()).append(SysConstants.COLUMN_SYMBOL).append(SysConstants.LINE_SYMBOL);
-				sb.append("备注：").append(vo.getRemark()).append(SysConstants.COLUMN_SYMBOL).append(SysConstants.LINE_SYMBOL);
-				sb.append("网盘链接：").append(vo.getLink()).append(SysConstants.COLUMN_SYMBOL).append(SysConstants.LINE_SYMBOL);
-				sb.append("网盘密码：").append(vo.getPw());
+				sb.append("歌手：").append(vo.getSinger()).append(SysConstants.LINE_SYMBOL);
+				sb.append("歌曲：").append(vo.getTitle()).append(SysConstants.LINE_SYMBOL);
+				sb.append("专辑：").append(vo.getAlbum()).append(SysConstants.LINE_SYMBOL);
+				sb.append("大小：").append(vo.getSize()).append(SysConstants.LINE_SYMBOL);
+				sb.append("语言：").append(vo.getLanguage()).append(SysConstants.LINE_SYMBOL);
+				sb.append("备注：").append(vo.getRemark()).append(SysConstants.LINE_SYMBOL);
+				sb.append("网盘链接：").append(vo.getLink()).append(SysConstants.LINE_SYMBOL);
+				sb.append("网盘密码：").append(vo.getPw()).append(SysConstants.LINE_SYMBOL);
+				sb.append("网页url：").append(vo.getUrl());
 				textArea.setText(sb.toString());
 			} catch (Exception e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
-				logger.error("",e1);
+				logger.error("", e1);
 			}
 		}
-		
+
 	};
-	
+
 	private JPanel eastPanel() {
 		JPanel eastPanel = new JPanel();
 		eastPanel.setLayout(new BorderLayout());
 
-		JButton testButton = new JButton("下载");
-		testButton.addActionListener(new createVOAction());
-		JPanel buttonPanel = new JPanel();
-		buttonPanel.setLayout(new GridLayout(18, 1));
-		buttonPanel.add(testButton);		
+		JButton pageUpButton = new JButton("上页");
+		pageUpButton.addActionListener(new pageUpAction());
 		
+		JButton pageDownButton = new JButton("下页");
+		pageDownButton.addActionListener(new pageDownAction());
+		
+		JButton downloadButton = new JButton("下载");
+		downloadButton.addActionListener(new downloadAction());
+		
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new GridLayout(14, 1,0,5));
+		buttonPanel.add(pageUpButton);
+		buttonPanel.add(pageDownButton);
+		buttonPanel.add(downloadButton);
+
 		eastPanel.add(buttonPanel, BorderLayout.CENTER);
-		eastPanel.setBorder(BorderFactory.createEmptyBorder(10, 5,
-				5, 10));
+		eastPanel.setBorder(BorderFactory.createEmptyBorder(10, 5, 5, 10));
 		return eastPanel;
 	}
-	
+
 	private JPanel southPanel() {
 		JPanel southPanel = new JPanel(new GridLayout(1, 1));
 		southPanel.setBorder(BorderFactory.createEmptyBorder(5, 10, 10, 10));
 
-//		textArea = new JTextArea(10, 60);
-//		textArea.setAutoscrolls(true);
-//		textArea.setEditable(false);
-//		JTextPane textPane = new JTextPane();
+		// textArea = new JTextArea(10, 60);
+		// textArea.setAutoscrolls(true);
+		// textArea.setEditable(false);
+		// JTextPane textPane = new JTextPane();
 
 		textArea = new JTextArea(4, 30);
-//		textField.setText("fdfdfdf");
+		// textField.setText("fdfdfdf");
 		textArea.setEditable(false);
-//		textArea.setColumns(30);
-//		textArea.setAutoscrolls(true);
-		textArea.setLineWrap(true);    
+		// textArea.setColumns(30);
+		// textArea.setAutoscrolls(true);
+		textArea.setLineWrap(true);
 		textArea.setWrapStyleWord(true);
-//		southPanel.setMinimumSize(new Dimension(500, 200));
-		
-		southPanel.setPreferredSize(new Dimension(550, 150));
-//		southPanel.setAutoscrolls(true);
+		// southPanel.setMinimumSize(new Dimension(500, 200));
+
+		southPanel.setPreferredSize(new Dimension(550, 170));
+		// southPanel.setAutoscrolls(true);
 		southPanel.add(new JScrollPane(textArea));
 
 		return southPanel;
+	}
+
+	private static class CloseWindowListener extends WindowAdapter {
+		public void windowClosing(WindowEvent e) {
+			H2DBServer.stop();
+			System.exit(0);
+		}
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		H2DBServer.start();
 		new MainFrame().drawFrame();
 	}
 
