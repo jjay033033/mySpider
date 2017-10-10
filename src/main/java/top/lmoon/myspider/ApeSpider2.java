4413d304b54b2e0bcd855abf7b44c52d4fc8d054
LMoon <jjay033033@gmail.com>
2017-10-10 09:55:45 +0800

    1

--- src/main/java/top/lmoon/myspider/ApeSpider2.java
+++ src/main/java/top/lmoon/myspider/ApeSpider2.java
@@ -1,13 +1,19 @@
 package top.lmoon.myspider;
 
+import top.lmoon.myspider.h2db.H2DBServer;
+import top.lmoon.myspider.pipeline.DBPipeline;
 import top.lmoon.myspider.pipeline.FilePipeline;
+import top.lmoon.myspider.util.CommonUtil;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.ResultItems;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.PageProcessor;
+import us.codecraft.webmagic.scheduler.FileCacheQueueScheduler;
 
 public class ApeSpider2 implements PageProcessor {
+
+	private static final String CURSOR_FILE_PATH = "./res/cursor/";
 
 	private Site site = Site.me().setRetryTimes(3).setSleepTime(1000);
 
@@ -17,36 +23,36 @@ public class ApeSpider2 implements PageProcessor {
 		int i = -1;
 		if ((i = remark.lastIndexOf("_")) > -1) {
 			remark = remark.substring(0, i);
-		}		
+		}
 		String singer = "";
 		String title = "";
 		String[] remarks = remark.split(" - ");
-		if(remarks.length>1){
-			singer = remarks[0].replace("/", "&").replace("\\", "*");
+		if (remarks.length > 1) {
+			singer = CommonUtil.replacePathSpecilChar(remarks[0]);
 			int indexTitle = remarks[1].indexOf(".");
-			if(indexTitle>-1){
+			if (indexTitle > -1) {
 				title = remarks[1].substring(0, indexTitle);
 			}
-		}		
+		}
 		String link = page.getHtml().xpath("//div[@class='fl over w638']/a/@href").toString();
 		String pw = page.getHtml().xpath("//div[@class='fl over w638']/b[@class='mt_1 yh d_b']/regex('密码：(\\w{4})',1)")
-				.toString();		
-		String album = page.getHtml().xpath("//div[@class='fl over w638']/h3/regex('选自专辑《([^》]*)》',1)")
 				.toString();
-		String size = page.getHtml().xpath("//div[@class='fl over w638']/h3[3]/text()")
-				.toString();
-		String language = page.getHtml().xpath("//div[@class='fl over w638']/h3[4]/text()")
-				.toString();
-		
-		page.putField("remark", remark);
-		page.putField("singer", singer);
-		page.putField("title", title);
-		page.putField("link", link);
-		page.putField("pw", pw);
-		page.putField("album", album);
-		page.putField("size", size);
-		page.putField("language", language);
-		
+		String album = page.getHtml().xpath("//div[@class='fl over w638']/h3/regex('选自专辑《([^》]*)》',1)").toString();
+		String size = page.getHtml().xpath("//div[@class='fl over w638']/h3[3]/text()").toString();
+		String language = page.getHtml().xpath("//div[@class='fl over w638']/h3[4]/text()").toString();
+		String url = page.getUrl().get();
+
+		// 所有字符串过滤换行符换列符
+		page.putField("remark", CommonUtil.replaceJavaSpecilChar(remark));
+		page.putField("singer", CommonUtil.replaceJavaSpecilChar(singer));
+		page.putField("title", CommonUtil.replaceJavaSpecilChar(title));
+		page.putField("link", CommonUtil.replaceJavaSpecilChar(link));
+		page.putField("pw", CommonUtil.replaceJavaSpecilChar(pw));
+		page.putField("album", CommonUtil.replaceJavaSpecilChar(album));
+		page.putField("size", CommonUtil.replaceJavaSpecilChar(size));
+		page.putField("language", CommonUtil.replaceJavaSpecilChar(language));
+		page.putField("url", url);
+
 		ResultItems resultItems = page.getResultItems();
 		if (resultItems.get("title") == null || resultItems.get("link") == null) {
 			// skip this page
@@ -57,10 +63,15 @@ public class ApeSpider2 implements PageProcessor {
 	public Site getSite() {
 		return site;
 	}
+	
+	public static void run(){
+		Spider.create(new ApeSpider2()).addUrl("http://www.51ape.com").addPipeline(new DBPipeline())
+		.setScheduler(new FileCacheQueueScheduler(CURSOR_FILE_PATH)).thread(5).run();
+	}
 
 	public static void main(String[] args) {
 		System.out.println("started!");
-		Spider.create(new ApeSpider2()).addUrl("http://www.51ape.com").addPipeline(new FilePipeline()).thread(5)
-				.run();
+		Spider.create(new ApeSpider2()).addUrl("http://www.51ape.com").addPipeline(new FilePipeline())
+				.setScheduler(new FileCacheQueueScheduler(CURSOR_FILE_PATH)).thread(5).run();
 	}
 }
